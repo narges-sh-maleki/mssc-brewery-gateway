51ff77b5bdfb5bc3bffa25ea89377e85f9111f25
narges_sh_maleki <narges.sh.maleki@gmail.com>
2021-01-18 13:46:00 +0100

    using application.yml to set routing

--- src/main/java/com/sfg/beerworks/gateway/config/LoadBalancedRouteConfig.java
+++ src/main/java/com/sfg/beerworks/gateway/config/LoadBalancedRouteConfig.java
@@ -3,14 +3,14 @@ package com.sfg.beerworks.gateway.config;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Profile("local-discovery")
-@Configuration
+//@Configuration
 public class LoadBalancedRouteConfig {
     @Bean
     public RouteLocator loadBalancedRouteLocator(RouteLocatorBuilder builder){
+
         return builder.routes()
                 .route("beer-service",r -> r
                         .path("/api/v1/beer/*","/api/v1/beer/beerUpc/*","/api/v1/beer*")
